c4ec6174dd4a4cd58312e0960f314a36ca16b0af
Clément Ramirez <clement.ramirez@bootlin.com>
2023-07-26 10:54:03 +0200

    full-sequence-beagply: Update configuration
    Configuration updated to work with beagleplay board.
    
    Add kernel-fetch-source and kernel-cross-compile labs to test sequence
    
    Signed-off-by: Clément Ramirez <clement.ramirez@bootlin.com>

--- full-sequence-scripts/embedded-linux-labs-test.sh
+++ full-sequence-scripts/embedded-linux-labs-test.sh
@@ -1,7 +1,8 @@
 # Training info
-export LABBOARD="stm32mp1"
-export SESSION_NAME="embedded-linux"
+export LABBOARD="beagleplay"
+export SESSION_NAME="embedded-linux-beagleplay"
 export LAB_URL="https://bootlin.com/doc/training/embedded-linux/embedded-linux-labs.tar.xz"
+export LAB_KERNEL_VERSION=6.4
 
 export LAB_DIR=$PWD/out/$SESSION_NAME-labs
 
@@ -11,3 +12,5 @@ cd lab-scripts
 ./setup.sh
 ./sysdev-toolchain.sh
 ./sysdev-u-boot-beagply.sh
+./sysdev-kernel-fetch-sources.sh
+./sysdev-kernel-cross-compiling.sh
