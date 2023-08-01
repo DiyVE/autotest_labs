4d697dcb8fa359811c84e70fa29c2e40f8cbff25
Clément Ramirez <clement.ramirez@bootlin.com>
2023-08-01 14:48:38 +0200

    sysdev-setup: rename and adapt
    Rename setup script to be training specific
    Adapt the script to new the top level system

--- lab-scripts/sysdev-setup.sh
+++ lab-scripts/sysdev-setup.sh
@@ -7,7 +7,7 @@ Help ()
 	echo " Setup lab data help "
 	echo
 	echo "Syntax: $0 [option...]"
-	echo "    -d, --lab-dir      Indicates where the lab will be stored"
+	echo "    -o, --output-dir      Indicates where the lab will be stored"
 	echo "    -u, --url          Indicates the url to download the labs-data"
 	echo "                         This url has to point a tar file"
 	echo 
@@ -21,7 +21,7 @@ while [ $# -gt 0 ]; do
 			LAB_URL="$2"
 			shift
 			shift;;
-		(-d|--lab-dir)
+		(-o|--lab-dir)
 			LAB_DIR="$2"
 			shift
 			shift;;
@@ -46,9 +46,9 @@ then
 fi
 
 # Tests if LAB_URL and SESSION_NAME has been set
-if [ -z $LAB_URL ] || [ -z $LAB_DIR ]
+if [ -z $LAB_DIR ]
 then
-	echo "LAB_URL and/or LAB_DIR variables need to be set"
+	echo "LAB_DIR variable needs to be set"
 	echo
 	Help
 	exit 1
@@ -63,13 +63,13 @@ sudo apt dist-upgrade -y
 # Picked from the official Crosstool-NG Dockerfile
 sudo apt install -y gcc g++ gperf bison flex texinfo help2man make libncurses5-dev \
 	python3-dev autoconf automake libtool libtool-bin gawk wget bzip2 xz-utils unzip \
-	patch libstdc++6 rsync git meson ninja-build bc
+	patch libstdc++6 rsync git meson ninja-build bc cpio kmod
 
+if [ ! -z "$LAB_URL" ]
+then
+	BASE_NAME="$(echo $LAB_URL | sed -e 's/^.*.\///g')"
+	wget $LAB_URL -O /tmp/$BASE_NAME
 
-BASE_NAME="$(echo $LAB_URL | sed -e 's/^.*.\///g')"
-wget $LAB_URL -O /tmp/$BASE_NAME
-
-mkdir -p $LAB_DIR
-
-# Extract the the tar file name and decompress it
-tar xvf /tmp/$BASE_NAME -C $LAB_DIR --strip 1
+	# Extract the the tar file name and decompress it
+	tar xvf /tmp/$BASE_NAME -C $LAB_DIR --strip 1
+fi
